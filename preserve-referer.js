efd1a9b9ca3c569c941ad4af60f8aff88d50c99b
Manuel Reimer <manuel.reimer@gmx.de>
2019-04-07 11:23:41 +0200

    Make preserve-referer.js work with Firefox
    Most probably I'm missing something as my solution is significantly simpler than the original variant, but mine does work with the "web build" of PDF.js.

--- preserve-referer.js
+++ preserve-referer.js
@@ -74,94 +74,36 @@ chrome.tabs.onRemoved.addListener(function(tabId) {
   delete g_referrers[tabId];
 });
 
-// This method binds a webRequest event handler which adds the Referer header
-// to matching PDF resource requests (only if the Referer is non-empty). The
-// handler is removed as soon as the PDF viewer frame is unloaded.
-chrome.runtime.onConnect.addListener(function onReceivePort(port) {
-  if (port.name !== 'chromecom-referrer') {
-    return;
-  }
-  // Note: sender.frameId is only set in Chrome 41+.
-  if (!('frameId' in port.sender)) {
-    port.disconnect();
-    return;
-  }
-  var tabId = port.sender.tab.id;
-  var frameId = port.sender.frameId;
 
-  // If the PDF is viewed for the first time, then the referer will be set here.
-  var referer = g_referrers[tabId] && g_referrers[tabId][frameId] || '';
-  port.onMessage.addListener(function(data) {
-    // If the viewer was opened directly (without opening a PDF URL first), then
-    // the background script does not know about g_referrers, but the viewer may
-    // know about the referer if stored in the history state (see chromecom.js).
-    if (data.referer) {
-      referer = data.referer;
-    }
-    chrome.webRequest.onBeforeSendHeaders.removeListener(onBeforeSendHeaders);
-    if (referer) {
-      // Only add a blocking request handler if the referer has to be rewritten.
-      chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, {
-        urls: [data.requestUrl],
-        types: ['xmlhttprequest'],
-        tabId: tabId,
-      }, ['blocking', 'requestHeaders']);
-    }
-    // Acknowledge the message, and include the latest referer for this frame.
-    port.postMessage(referer);
-  });
+// Add the Referer header to the "fetch" request done by PDF.js
+browser.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    // Don't mess with any request not done by our extension
+    if (details.originUrl.indexOf(browser.extension.getURL('/'))  !== 0)
+      return;
 
-  // The port is only disconnected when the other end reloads.
-  port.onDisconnect.addListener(function() {
-    if (g_referrers[tabId]) {
-      delete g_referrers[tabId][frameId];
-    }
-    chrome.webRequest.onBeforeSendHeaders.removeListener(onBeforeSendHeaders);
-    chrome.webRequest.onHeadersReceived.removeListener(exposeOnHeadersReceived);
-  });
+    // Nothing to do if no Referer was saved
+    var referer = g_referrers[details.tabId] && g_referrers[details.tabId][details.frameId] || '';
+    if (referer === '')
+      return;
 
-  // Expose some response headers for fetch API calls from PDF.js;
-  // This is a work-around for https://crbug.com/784528
-  chrome.webRequest.onHeadersReceived.addListener(exposeOnHeadersReceived, {
-    urls: ['https://*/*'],
+    // Add or modify Referer header
+    let found = false;
+    details.requestHeaders.forEach(function(header){
+      if (header.name.toLowerCase() == "referer") {
+        header.value = referer;
+        found = true;
+      }
+    });
+    if (!found)
+      details.requestHeaders.push({"name": "Referer", "value": referer});
+
+    return {requestHeaders: details.requestHeaders};
+  },
+  {
+    urls: [
+      '<all_urls>'
+    ],
     types: ['xmlhttprequest'],
-    tabId: tabId,
-  }, ['blocking', 'responseHeaders']);
-
-  function onBeforeSendHeaders(details) {
-    if (details.frameId !== frameId) {
-      return;
-    }
-    var headers = details.requestHeaders;
-    var refererHeader = getHeaderFromHeaders(headers, 'referer');
-    if (!refererHeader) {
-      refererHeader = { name: 'Referer', };
-      headers.push(refererHeader);
-    } else if (refererHeader.value &&
-        refererHeader.value.lastIndexOf('chrome-extension:', 0) !== 0) {
-      // Sanity check. If the referer is set, and the value is not the URL of
-      // this extension, then the request was not initiated by this extension.
-      return;
-    }
-    refererHeader.value = referer;
-    return { requestHeaders: headers, };
-  }
-
-  function exposeOnHeadersReceived(details) {
-    if (details.frameId !== frameId) {
-      return;
-    }
-    var headers = details.responseHeaders;
-    var aceh = getHeaderFromHeaders(headers, 'access-control-expose-headers');
-    // List of headers that PDF.js uses in src/display/network_utils.js
-    var acehValue =
-      'accept-ranges,content-encoding,content-length,content-disposition';
-    if (aceh) {
-      aceh.value += ',' + acehValue;
-    } else {
-      aceh = { name: 'Access-Control-Expose-Headers', value: acehValue, };
-      headers.push(aceh);
-    }
-    return { responseHeaders: headers, };
-  }
-});
+  },
+  ['blocking', "requestHeaders"]);
