2c9a056b6bf1b8100300cd1121bc65a6f55418bb
Manuel Reimer <manuel.reimer@gmx.de>
2019-04-27 17:09:57 +0200

    Make zoom dependent on gesture distance

--- patches/pdfjs-pinch-gestures.js
+++ patches/pdfjs-pinch-gestures.js
@@ -2,6 +2,8 @@
 // Pinch gesture workaround for Firefox for Android
 // https://github.com/M-Reimer/android-pdf-js
 (function AndroidPDFJSPinchGesture() {
+  const STEP_DIST = 50;
+
   function PinchGestureDist(aTouches) {
     return Math.sqrt(Math.pow((aTouches[1].pageX - aTouches[0].pageX),2)+Math.pow((aTouches[1].pageY - aTouches[0].pageY),2));
   }
@@ -18,16 +20,14 @@
   });
   document.addEventListener('touchend', function (e) {
     if (e.touches.length === 0 && starttouches && endtouches) {
-      const start = PinchGestureDist(starttouches);
-      const end = PinchGestureDist(endtouches);
+      const d = PinchGestureDist(endtouches) - PinchGestureDist(starttouches);
 
-      if (Math.abs(start - end) < 50)
-        return;
-
-      if (start > end)
-        document.getElementById('zoomOut').click();
-      else if (start < end)
-        document.getElementById('zoomIn').click();
+      for (let loop = 0; loop < Math.abs(Math.floor(d / STEP_DIST)); loop++) {
+        if (d < 0)
+          document.getElementById('zoomOut').click();
+        else if (d > 0)
+          document.getElementById('zoomIn').click();
+      }
 
       starttouches = undefined;
       endtouches = undefined;
